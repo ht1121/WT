eb431ed6983cf57d61500879a18bcb58b8ae2eef
ht1121 <hanseltai3@gmail.com>
2017-05-29 18:43:43 +0700

    ;
    ;

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
 </head>
 <body>
 <div>
+
+<img src="" ></img>
 <h1><a href="">Banyaknya sampah di UC</a></h1>
 <p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
 
 <h1><a href="">Bocah UC pertama yang memiliki Anak</a></h1>
 <p>Kevin Pratama adalah Bocah UC pertama yang memiliki anak. anak tersebut bernama ferry tetapi herannya tidak menyerupai orang tuanya. Single Father</p>
 
-<h1><a href="">Banyaknya sampah di UC</a></h1>
-<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
+<h1><a href="">Kurangnya gaji dosen</a></h1>
+<p>Dosen pada Marah karena kekurangan gaji di universitas ciputra walaupun mereka kerja dari pagi hingga malam</p>
 
-<h1><a href="">Banyaknya sampah di UC</a></h1>
-<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
+<h1><a href="">Makanan di UC walk mahal</a></h1>
+<p>Murid-murid UC menderita karena makanan yang terlalu mahal apalagi untuk mahasiswa yang kekurangan uang untuk makan siang mereka</p>
 </div>
 </body>
 </html>
