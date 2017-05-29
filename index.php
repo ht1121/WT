ab6ed82bbecfe80096f4ba7f5c2c31267b655e71
ht1121 <hanseltai3@gmail.com>
2017-05-29 15:41:11 +0700

    kp punya anak

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>UC News</title>
+	<title><h1>UC News</h1></title>
 </head>
 <body>
+<h1><a href="">Banyaknya sampah di UC</a></h1>
+<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
 
+<h1><a href="">Bocah UC pertama yang memiliki Anak</a></h1>
+<p>Kevin Pratama adalah Bocah UC pertama yang memiliki anak. anak tersebut bernama ferry tetapi herannya tidak menyerupai orang tuanya. Single Father</p>
+
+<h1><a href="">Banyaknya sampah di UC</a></h1>
+<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
+
+<h1><a href="">Banyaknya sampah di UC</a></h1>
+<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
 </body>
 </html>
