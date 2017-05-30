ae75813ad107487e9048cca34d2d027b41e75ad5
kp1304 <kevinsoedarso@gmail.com>
2017-05-30 08:01:47 +0700

    aa

--- index.php
+++ index.php
@@ -5,29 +5,23 @@
 <!DOCTYPE html>
 <link href="style.css" rel="stylesheet">
 <html>
-<head>
-	<title>UC News</title>
-</head>
-<body>
 <div>
-
-<img src="" ></img>
-<h1><a href="">Banyaknya sampah di UC</a></h1>
-=======
-<h1>UC Flash News</h1>
-<div id="head1">
-<img id="img/1.jpg" img src="img/1.jpg" alt="Sampah di UC" style="width:50px;height:50px">
-<span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
-<img class="modal-content" id="img/1.jpg">
-<div id="caption"></div>
-<h2><a href="">Banyaknya sampah di UC</a></h2>
-<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
-</div>
-<h2><a href="">Bocah UC pertama yang memiliki Anak</a></h2>
-<p>Kevin Pratama adalah Bocah UC pertama yang memiliki anak. anak tersebut bernama ferry tetapi herannya tidak menyerupai orang tuanya. Single Father</p>
-
-<<<<<<< HEAD
-<h1><a href="">Kurangnya gaji dosen</a></h1>
+	<head>
+		<title>UC News</title>
+	</head>
+	<body>
+		<h1>UC Flash News</h1>
+			<div id="head1">
+				<img id="img/1.jpg" img src="img/1.jpg" alt="Sampah di UC" style="width:50px;height:50px">
+						<span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+							<img class="modal-content" id="img/1.jpg">
+						<div id="caption"></div>
+					<h2><a href="">Banyaknya sampah di UC</a></h2>
+				<p>Hari ini, hari yang cerah di UC. banyak anak-anak UC yang datang ke UC namun sampah-sampah di UC pun juga banyak</p>
+			</div>
+		<h2><a href="">Bocah UC pertama yang memiliki Anak</a></h2>
+	<p>Kevin Pratama adalah Bocah UC pertama yang memiliki anak. anak tersebut bernama ferry tetapi herannya tidak menyerupai orang tuanya. Single Father</p>
+	<h1><a href="">Kurangnya gaji dosen</a></h1>
 <p>Dosen pada Marah karena kekurangan gaji di universitas ciputra walaupun mereka kerja dari pagi hingga malam</p>
 
 <h1><a href="">Makanan di UC walk mahal</a></h1>
